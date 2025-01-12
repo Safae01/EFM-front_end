6d38525fcea1c2a7f163c49707dd45a47085629b
Safae01 <111088352+Safae01@users.noreply.github.com>
2025-01-12 21:57:41 +0100

    B

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
 fetch('data.json')
 .then((reponse)=>reponse.json())
 .then((data)=>{
-    const distance = document.getElementById('distance');
-    Object.keys(data).forEach(element => {
-        const option = document.createElement('option')
-        option.value=element;
-        option.textContent=element;
-        distance.appendChild(option)
-    });
+    const distanceSelect = document.getElementById('distance');
+    Object.keys(data.tarifs.standard.distance).forEach((element)=>{
+            const option = document.createElement('option')
+            option.value=element;
+            option.textContent=element;
+            distanceSelect.appendChild(option)
+    })
+    
+    
+    const btn = document.querySelector('.btn-calcule');
+
+    btn.addEventListener("click",()=>{
+        const section =document.querySelector('.section4')
+
+        const result = document.getElementById('result')
+        const resultE= document.getElementById('resultE')
+        const poid = document.getElementById('poids').value
+        const longueur = document.getElementById('longueur').value
+        const largeur = document.getElementById('largeur').value
+        const hauteur = document.getElementById('hauteur').value
+        const distanceKey = distanceSelect.value
+        const distanceValueS = data.tarifs.standard.distance[distanceKey]
+        const distanceValueE= data.tarifs.express.distance[distanceKey]
+        
+
+        const baseS = data.tarifs.standard.base;
+        const poidsS = data.tarifs.standard.poids;
+        const dimensionS =data.tarifs.standard.dimension
+        const baseE = data.tarifs.express.base;
+        const poidsE = data.tarifs.express.poids;
+        const dimensionE =data.tarifs.express.dimension
+        
+
+        if(poid === '' || longueur === '' || largeur === "" || hauteur === "" || distanceKey === ""){
+            alert('veuillez remplir tous les champs')
+        }else{
+            // setTimeout(() => {
+            //     section.scrollIntoView({
+            //         behavior: "smooth"
+            //     })
+            // }, 100)
+            section.scrollIntoView({
+                behavior:"smooth"
+            })
+            section.style.display='flex'
+            result.textContent= baseS+(poid*poidsS)+(longueur*dimensionS)+distanceValueS
+            resultE.textContent= baseE+(poid*poidsE)+(longueur*dimensionE)+distanceValueE
+        }
+    })
 })
